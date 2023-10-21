3c5db17423c9a56dfc5a1b74ff52a69ff3386724
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2023-10-21 15:10:34 +0200

    Small changes that went unnoticed

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 function Page(props: any) {
     return <div>
         {props.header}
-        <Container style={{paddingTop: 16}} maxWidth='sm'>
+        <Container style={{paddingTop: 16}} maxWidth='md'>
             <Stack spacing={2}>
                 {props.children}
             </Stack>
