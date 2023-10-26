7537f2c458d0bf801bb94a40010f69c5b427e149
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2023-10-26 10:59:50 +0200

    Info about work anywhere

--- src/components/WorkDisplay.tsx
+++ src/components/WorkDisplay.tsx
@@ -1,14 +1,29 @@
-import { ListItemButton, ListItemText } from "@mui/material";
+import { IconButton, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import HelpIcon from '@mui/icons-material/Help';
 
 function WorkDisplay(props: any) {
-    return <ListItemButton
-        selected={props.selected}
-        onClick={props.onClick}
-    >
-        <ListItemText
-            primary={`${props.work.title}${props.work.durationMinutes > 0 && props.showTime ? ' - ' + props.work.durationMinutes + ' perc' : ''}`}
-        />
-    </ListItemButton>;
+    return <ListItem
+        key={props.work.id}
+        secondaryAction={
+            <IconButton
+                edge='end'
+                onClick={() => {
+                    alert(props.work.description);
+                }}
+            >
+                <HelpIcon />
+            </IconButton>
+        }>
+        <ListItemButton
+            selected={props.selected}
+            onClick={props.onClick}
+        >
+            <ListItemText
+                primary={props.work.title}
+                secondary={`${props.work.durationMinutes > 0 && props.showTime ? props.work.durationMinutes + ' perc' : ''}`}
+            />
+        </ListItemButton>
+    </ListItem>;
 }
 
 export default WorkDisplay;
